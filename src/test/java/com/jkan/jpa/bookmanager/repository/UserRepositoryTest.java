b5ff6e6bd7e37604ced8f414d84cc362d2070cbd
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-14 00:01:38 +0900

    jpa debug console yml 추가 + order by 추가

--- src/test/java/com/jkan/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jkan/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -4,6 +4,9 @@ import com.jkan.jpa.bookmanager.domain.Users;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -13,9 +16,12 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        userRepository.save(new Users());
-
-        //findAll은 지양한다. 천만건이 있으면 천만건을 다 올려서 out of memory 발생 여지있음.
-        System.out.println(userRepository.findAll());
+        List<Users> users = userRepository.findAll(Sort.by(Sort.Direction.DESC,"name"));
+        users.forEach(System.out::println);
+//
+//        userRepository.save(new Users());
+//
+//        //findAll은 지양한다. 천만건이 있으면 천만건을 다 올려서 out of memory 발생 여지있음.
+//        userRepository.findAll().forEach(System.out::println);
     }
 }
