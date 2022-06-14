4a9c9f3ffb3b92065a5634ae2dc2a3d71ceb09d5
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-15 00:03:02 +0900

    jpa Page 구현체 정리

--- src/test/java/com/jkan/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jkan/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,6 +5,9 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
@@ -19,9 +22,16 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    @Transactional
     void crud() {
-        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
+        //제로베이스이다!
+        Page<Users> users = userRepository.findAll(PageRequest.of(0, 3));
 
+        System.out.println("users:" + users);
+
+        System.out.println("totalElements : " + users.getTotalElements());
+        System.out.println("totalPages : " + users.getTotalPages());
+        System.out.println("numberOfElements : " + users.getNumberOfElements());
+        System.out.println("sort : "+ users.getSort());
+        System.out.println("size : "+ users.getSize());
     }
 }
