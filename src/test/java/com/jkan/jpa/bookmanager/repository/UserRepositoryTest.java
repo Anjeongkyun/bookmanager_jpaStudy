618d8cd24fa4cd1cb020bb204bbd33d54d4abaab
Anjeongkyun <dkswjdrbs12@naver.com>
2022-06-15 00:08:26 +0900

    ExampleMatcher를 사용하여 비교 및 Ignore 기능 적용

--- src/test/java/com/jkan/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/jkan/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,14 +5,13 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
 
 @SpringBootTest
@@ -23,15 +22,13 @@ class UserRepositoryTest {
 
     @Test
     void crud() {
-        //제로베이스이다!
-        Page<Users> users = userRepository.findAll(PageRequest.of(0, 3));
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("name")
+                .withMatcher("email", endsWith());
 
-        System.out.println("users:" + users);
+        Example<Users> example = Example.of(new Users("ma", "naver.com"), matcher);
 
-        System.out.println("totalElements : " + users.getTotalElements());
-        System.out.println("totalPages : " + users.getTotalPages());
-        System.out.println("numberOfElements : " + users.getNumberOfElements());
-        System.out.println("sort : "+ users.getSort());
-        System.out.println("size : "+ users.getSize());
+        userRepository.findAll(example).forEach(System.out::println);
+
     }
 }
