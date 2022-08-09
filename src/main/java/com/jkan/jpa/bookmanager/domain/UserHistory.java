15a3116d8de28535a499c507248d76aacb33d9e2
Anjeongkyun <dkswjdrbs12@naver.com>
2022-08-10 00:50:43 +0900

    주석 삭제

--- src/main/java/com/jkan/jpa/bookmanager/domain/UserHistory.java
+++ src/main/java/com/jkan/jpa/bookmanager/domain/UserHistory.java
@@ -22,9 +22,6 @@ public class UserHistory  extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column(name = "user_id", insertable = false, updatable = false)
-//    private Long userId;
-
     private String name;
 
     private String email;
